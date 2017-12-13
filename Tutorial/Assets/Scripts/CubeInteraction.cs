903a38bf4c8609be8540dfb5723f4ee9c295a758
Balázs <bigyo4u85@gmail.com>
2017-12-13 16:15:13 +0100

    Almost empty sample project as a starting point for Daydream development with Unity.

---
+++ Tutorial/Assets/Scripts/CubeInteraction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CubeInteraction : MonoBehaviour {
+
+    public float RotationSpeed = 100f;
+    private Material material;
+    private Color activated = Color.green;
+    private Color selected = Color.blue;
+    private Color idle = Color.gray;
+
+    private bool IsActive { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<Renderer>().material;
+        IsActive = false;
+        ChangeColor(idle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsActive)
+        {
+            float yRotation = Time.deltaTime * RotationSpeed;
+            transform.Rotate(new Vector3(0f, yRotation, 0f));
+        }
+	}
+
+    public void OnActivated()
+    {
+        IsActive = !IsActive;
+        ChangeColor(IsActive ? activated : selected);
+    }
+
+    public void OnSelected(bool isSelected)
+    {
+        if (!IsActive)
+        {
+            ChangeColor(isSelected ? selected : idle);
+        }
+    }
+
+    private void ChangeColor(Color color)
+    {
+        material.color = color;
+    }
+}
